b190a554df7e6ff8480a3a2d55a24d78f1ab28f2
Sherifat Samodi <sherifat444@gmail.com>
2025-04-16 01:16:04 +0100

    Initial commit

--- budget_tracker/budget.py
+++ budget_tracker/budget.py
@@ -1,22 +1,59 @@
 # TODO: Import Transaction class from transaction.py
+from transaction import Transaction
 
 class Budget:
     def __init__(self):
         # TODO: Create a list to store transactions
-        pass
+        self.transactions = []
+        # [Transaction("ads", 500, "work", True), Transaction("ads", 500, "work", True)]
 
     def add_transaction(self, transaction):
         # TODO: Append the transaction to the list
-        pass
+        self.transactions.append(transaction)
 
     def get_balance(self):
         # TODO: Loop through transactions and calculate the balance
-        pass
+        balance = 0
+        for transaction in self.transactions: 
+            if transaction.is_income:  
+                balance += transaction.amount
+            else:
+                balance -= transaction.amount
+        return balance
 
     def save_to_file(self, filename):
         # TODO: Write transactions to a file
-        pass
+        with open(filename, 'w') as file:
+            for transaction in self.transactions:
+                file.write(str(transaction))
+                file.write("\n")
 
     def load_from_file(self, filename):
         # TODO: Read file and load transactions using Transaction class
-        pass
+        with open(filename, "r") as file:
+            transactions_with_newline = file.readlines()
+            for transaction in transactions_with_newline:
+                # 'Income: Freelance | £500 | Category: Work '
+                transaction_without_newline = transaction[:-1]
+
+                # ["Income: Freelance ", " �500 ", " Category: Work"]
+                transaction_bits = transaction_without_newline.split("|")
+
+                is_income_and_description = transaction_bits[0].strip()
+                amount = transaction_bits[1].strip()
+                category = transaction_bits[2].strip()
+
+                # Get is_income and description details from "Income: Freelance"
+                is_income, description = is_income_and_description.split(":")
+                is_income = True if is_income == "Income" else False
+                description = description.strip()
+
+                # Get the amount details from "�500"
+                amount = int(amount[1:])
+
+                # Get the category details from "Category: Work"
+                category = category.split(":")[1].strip()
+
+                transaction = Transaction(description, amount, category, is_income)
+
+                self.transactions.append(transaction)
--- budget_tracker/main_budget.py
+++ budget_tracker/main_budget.py
@@ -1,8 +1,10 @@
 # TODO: Import Budget and Transaction classes
+from budget import Budget
+from transaction import Transaction
 
 def main():
     budget = Budget()
-    budget.load_from_file("budget_data.txt")
+    #budget.load_from_file("budget_data.txt")
 
     while True:
         print("\n1. Add Transaction\n2. View Summary\n3. Save & Exit")
@@ -10,13 +12,37 @@ def main():
 
         if choice == "1":
             # TODO: Get input and create Transaction
+            description = input("Enter Transaction Desc: ")
+            amount = int(input("Enter transaction amount: "))
+            category = input("Enter transaction category: ")
+            is_income = input("Is this an Income or Expense transaction ? Y(Income) | N(Expense): ")
+
+            if is_income == "Y":
+                is_income = True
+            else:
+                is_income = False
+
+            transaction = Transaction(
+                description,
+                amount,
+                category,
+                is_income,
+            )
+
             # TODO: Add transaction to budget
-            pass
+            budget.add_transaction(transaction)
 
         elif choice == "2":
             # TODO: Print each transaction
+            # transactions = budget.transactions
+            # for transaction in transactions:
+            #     print(transaction)
+            transactions = budget.transactions
+            for transaction in transactions:
+                print(transaction)
+
             # TODO: Print current balance
-            pass
+            print(f"Balance: £{budget.get_balance()}")
 
         elif choice == "3":
             budget.save_to_file("budget_data.txt")
